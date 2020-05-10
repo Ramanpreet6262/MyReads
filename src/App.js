7302cb7c5ccb344f0a91bd9150fbac484914859a
Ramanpreet6262 <ramanpreet6262@gmail.com>
2020-05-11 02:14:10 +0530

    added progress bar for loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import NProgress from 'nprogress';
 import Homepage from './Containers/Homepage/Homepage';
 import * as BooksApi from './BooksAPI';
 import Loader from './Components/Loader/Loader';
+import 'nprogress/nprogress.css';
 import './App.css';
 
 const App = () => {
@@ -11,21 +13,25 @@ const App = () => {
 
   useEffect(() => {
     setLoading(true);
+    NProgress.start();
     BooksApi.getAll()
       .then(res => {
-        console.log(res);
+        // console.log(res);
         setAllBooks(res);
         setLoading(false);
+        NProgress.done();
       })
       .catch(err => {
-        console.log(err);
+        // console.log(err);
         alert(`An error occurred while fetching books. Please retry`);
         setLoading(false);
+        NProgress.done();
       });
   }, []);
 
   const handleUpdate = (event, newBook) => {
     const shelf = event.target.value;
+    NProgress.start();
     const updatedAllBooks = allBooks.map(book => {
       if (book.id === newBook.id) {
         book.shelf = shelf;
@@ -35,10 +41,12 @@ const App = () => {
     setAllBooks(updatedAllBooks);
     BooksApi.update(newBook, shelf)
       .then(res => {
+        NProgress.done();
         alert(`${newBook.title} has been added to ${shelf} successfully!!`);
       })
       .catch(err => {
         console.log(err);
+        NProgress.done();
         alert(`An error occured while updating the shelf of ${newBook.title}`);
       });
   };
